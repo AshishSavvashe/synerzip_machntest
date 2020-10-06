fc2c57a9ab4ef2641a248e8b91044051dd0b9fd6
Ashish <savvasheashish1000@gmail.com>
2020-10-06 16:27:04 +0530

    done

--- app/src/main/java/demo/com/synerzip_ashish_savvashe/views/MainActivity.kt
+++ app/src/main/java/demo/com/synerzip_ashish_savvashe/views/MainActivity.kt
@@ -2,12 +2,15 @@ package demo.com.synerzip_ashish_savvashe.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import demo.com.synerzip_ashish_savvashe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
+
         //Check for instance and then add main fragment
         if (savedInstanceState == null) {
             supportFragmentManager
